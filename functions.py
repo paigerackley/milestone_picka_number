0df35e49f4c65ef0a9873e1631908a3907efd7ba
paigerackley <102173123+paigerackley@users.noreply.github.com>
2022-11-14 19:58:18 -0600

    finally done

--- functions.py
+++ functions.py
@@ -62,6 +62,17 @@ def player_choice(board):
         
     return position
 
+def win_check(board, mark):
+    
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
 def replay():
     
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
@@ -70,7 +81,7 @@ def replay():
 
 ## Putting it all together
 def the_game():
-print('Welcome to Tic Tac Toe')
+    print('Welcome to Tic Tac Toe')
 
 while True:
 
